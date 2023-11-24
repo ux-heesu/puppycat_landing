b45c02be413d6724b1434cafd83f22895e388919
ux-heesu <engine_hskim@uxplus.co.kr>
2023-11-25 00:31:31 +0900

    안늉

--- js/layout.js
+++ js/layout.js
@@ -2,6 +2,7 @@ function docPosition() {
     let docInner = document.querySelector('.docSlider-inner');
     docInner.style.marginLeft = -docInner.clientWidth / 2 + 'px';
 }
+
 window.addEventListener('load', function () {
     /*docslider*/
     docSlider.init({});
