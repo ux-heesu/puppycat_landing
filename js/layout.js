d44ae1a60c80ddd4a78d77076dca289d1cd8ada7
ux-heesu <engine_hskim@uxplus.co.kr>
2023-11-25 00:12:05 +0900

    레이아웃수정

--- js/layout.js
+++ js/layout.js
@@ -1,4 +1,13 @@
+function docPosition() {
+    let docInner = document.querySelector('.docSlider-inner');
+    docInner.style.marginLeft = -docInner.clientWidth / 2 + 'px';
+}
 window.addEventListener('load', function () {
-  /*docslider*/
-  docSlider.init({});
+    /*docslider*/
+    docSlider.init({});
+    docPosition();
+});
+
+window.addEventListener('resize', function () {
+    docPosition();
 });
