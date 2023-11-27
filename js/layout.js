41ddfe64aa545c0f2d9c85e7d7d1b4bc085ee2fd
ux-heesu <engine_hskim@uxplus.co.kr>
2023-11-27 16:11:46 +0900

    안드로이드,ios 구분테스트

--- js/layout.js
+++ js/layout.js
@@ -3,10 +3,36 @@ function docPosition() {
     docInner.style.marginLeft = -docInner.clientWidth / 2 + 'px';
 }
 
+function headerVisible() {
+    if (document.querySelector('.main-visual-col').classList.contains('docSlider-current')) {
+        document.querySelector('header').style.opacity = '0';
+    } else {
+        document.querySelector('header').style.opacity = '1';
+    }
+}
+
 window.addEventListener('load', function () {
     /*docslider*/
-    docSlider.init({});
+    docSlider.init({
+        afterChange: function () {
+            headerVisible();
+        },
+    });
     docPosition();
+    headerVisible();
+
+    document.querySelector('.lottie-cat').innerHTML = '<lottie-player src="img/lottie_puppy_cat.json" background="transparent" speed=".9" loop autoplay></lottie-player>';
+    document.querySelector('.lottie-dog').innerHTML = '<lottie-player src="img/lottie_puppy_dog.json" background="transparent" speed=".9" loop autoplay></lottie-player>';
+
+    let varUA = navigator.userAgent.toLowerCase();
+
+    if (varUA.indexOf('android') > -1) {
+        //안드로이드
+        console.log('안드로이드');
+    } else if (varUA.indexOf('iphone') > -1 || varUA.indexOf('ipad') > -1 || varUA.indexOf('ipod') > -1) {
+        //IOS
+        console.log('ios');
+    }
 });
 
 window.addEventListener('resize', function () {
