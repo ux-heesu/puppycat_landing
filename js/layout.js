538bc877c7bdc5817a90a3b240ed69098c1c5f28
seoungeunLee <118154731+seoungeunLee@users.noreply.github.com>
2023-11-28 13:02:55 +0900

    section3 롤링배너 fin

--- js/layout.js
+++ js/layout.js
@@ -36,6 +36,64 @@ function mobileCheck() {
     }
 }
 
+
+
+function startRoller(){
+    let betweenDistance = 1;//이동 크기 - 정수여야 함
+
+    originalID = window.setInterval(betweenRollCallback, parseInt(1000/80), betweenDistance, document.querySelector('#roller1'));
+    cloneID = window.setInterval(betweenRollCallback, parseInt(1000/80), betweenDistance, document.querySelector('#roller2'));
+        
+    original_reverseID = window.setInterval(betweenRollCallbackReverse, parseInt(1000/80), betweenDistance, document.querySelector('#roller-reverse1'));    
+    clone_reverseID = window.setInterval(betweenRollCallbackReverse, parseInt(1000/80), betweenDistance, document.querySelector('#roller-reverse2'));
+}
+
+//롤링 정지
+function stopRoller(){
+    clearInterval(originalID);
+    clearInterval(cloneID);
+    clearInterval(original_reverseID);
+    clearInterval(clone_reverseID);
+}
+
+
+
+//인터벌 애니메이션 함수(공용)
+function betweenRollCallback(d, roller){
+        //인터벌 메서드로 애니메이션 생성
+    let rollerWidth = document.querySelector('.roller ul').offsetWidth;//회전 배너 너비값
+    
+    let left = parseInt(roller.style.left);
+    roller.style.left = (left - d)+'px';//이동
+    //조건부 위치 리셋
+    if(rollerWidth + (left - d) <= 0){
+        roller.style.left = rollerWidth+'px';
+    }
+}
+
+//인터벌 애니메이션 함수(공용) - 반대로 롤링
+function betweenRollCallbackReverse(d, roller){
+    //인터벌 메서드로 애니메이션 생성
+    let rollerWidth = document.querySelector('.roller-reverse ul').offsetWidth;//회전 배너 너비값
+
+
+    let right = parseInt(roller.style.right);
+    roller.style.right = (right - d)+'px';//이동
+    //조건부 위치 리셋
+    if(rollerWidth + (right - d) <= 0){
+        roller.style.right = rollerWidth+'px';
+    }
+}
+
+
+
+
+
+
+
+var original_reverseID, clone_reverseID;//인터벌 포인터
+
+
 window.addEventListener('load', function () {
     /*docslider*/
     docSlider.init({
@@ -49,10 +107,55 @@ window.addEventListener('load', function () {
     document.querySelector('.lottie-cat').innerHTML = '<lottie-player src="img/lottie_puppy_cat.json" background="transparent" speed=".9" loop autoplay></lottie-player>';
     document.querySelector('.lottie-dog').innerHTML = '<lottie-player src="img/lottie_puppy_dog.json" background="transparent" speed=".9" loop autoplay></lottie-player>';
 
+    document.querySelector('.lottie-banner-cat').innerHTML = '<lottie-player src="img/lottie_puppy_banner_cat.json" background="transparent" speed=".9" loop autoplay></lottie-player>';
+    document.querySelector('.lottie-banner-round').innerHTML = '<lottie-player src="img/lottie_puppy_banner_round.json" background="transparent" speed=".9" loop autoplay></lottie-player>';
+
     mobileCheck();
+
+
+
+
+    //롤링 배너 원본 생성
+    let roller = document.querySelector('.roller');   
+    roller.id = 'roller1';
+  
+    let roller_reverse = document.querySelector('.roller-reverse');   
+    roller_reverse.id = 'roller-reverse1';
+
+    //롤링 배너 복제본 생성
+    let clone = roller.cloneNode(true);          
+    clone.id = 'roller2';
+  
+    let clone_reverse = roller_reverse.cloneNode(true);          
+    clone_reverse.id = 'roller-reverse2';
+
+    // 필요한 부분에 부착
+    document.querySelector('.rollingbanner .wrap').appendChild(clone);
+    document.querySelector('.wrap-reverse').appendChild(clone_reverse);
+
+
+
+    //원본, 복제본 배너 위치 지정
+    document.querySelector('#roller1').style.left = '0px';
+    document.querySelector('#roller2').style.left = document.querySelector('.roller ul').offsetWidth+'px';
+
+    document.querySelector('#roller-reverse1').style.right = '0px';
+    document.querySelector('#roller-reverse2').style.right = document.querySelector('.roller-reverse ul').offsetWidth+'px';
+
+    //클래스 할당
+    roller.classList.add('original');
+    clone.classList.add('clone');
+
+    roller_reverse.classList.add('original-reverse');
+    clone_reverse.classList.add('clone-reverse');
+
+    startRoller();
+
+
 });
 
 window.addEventListener('resize', function () {
     docPosition();
     mobileCheck();
+    
 });
